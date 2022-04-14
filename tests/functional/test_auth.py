b817b25db3e305a4182aa8504537dde6b514e290
felipeslanza <felipeslanza@gmail.com>
2022-04-14 13:43:16 -0300

    added auth tests; improved test_methods; minor fix to gitignore

---
+++ tests/functional/test_auth.py
@@ -0,0 +1,61 @@
+import pytest
+
+from server.models import Trainer
+
+
+@pytest.fixture(scope="module")
+def trainer(client):
+    trainer_ = Trainer(
+        name="Jon",
+        email="j@j.com",
+        password="abcXYZ",
+        birthday="1990-01-01",
+    )
+
+    db = client.db
+    db.session.add(trainer_)
+    db.session.commit()
+
+    return trainer_
+
+
+@pytest.fixture
+def logged_trainer(trainer):
+    trainer.token = trainer.generate_token()
+    return trainer
+
+
+def test_expired_token(client, trainer):
+    token = trainer.generate_token(-5)  # expired
+    res = client.get(
+        f"/api/trainer/{trainer.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    assert res.status_code == 401
+
+
+def test_protected_routes_raise_401(client):
+    s1 = client.get("/api/trainer/1").status_code
+    s2 = client.put(
+        "/api/trainer/1",
+        json={"foo": "bar"},
+        headers={"Authorization": "Bearer bad-token"},
+    ).status_code
+    s3 = client.delete("/api/trainer/1").status_code
+
+    assert s1 == s2 == s3 == 401
+
+
+def test_successful_token_attempt(client, logged_trainer):
+    res = client.post(
+        "/api/login", json=dict(email=logged_trainer.email, password="abcXYZ")
+    )
+    assert "token" in res.json
+
+
+def test_failed_token_attempt(client, logged_trainer):
+    res = client.post(
+        "/api/login", json=dict(email=logged_trainer.email, password="hacker")
+    )
+    assert res.status_code == 401
--- tests/functional/test_methods.py
+++ tests/functional/test_methods.py
@@ -8,16 +8,61 @@ def post_trainer_data(client):
     return dict(name="Jon", email="j@j.com", password="123", birthday="1990-01-01")
 
 
+@pytest.fixture
+def logged_trainer(client, post_trainer_data):
+    trainer = Trainer.query.get(1)
+    trainer.token = trainer.generate_token()
+    return trainer
+
+
 def test_non_json_payload_raises(client, post_trainer_data):
-    res = client.post("/trainer", data=post_trainer_data)
+    res = client.post("/api/trainer", data=post_trainer_data)
     assert res.status_code == 400
 
 
-def test_trainer_creation(client, post_trainer_data):
+def test_new_trainer_post(client, post_trainer_data):
     trainer = Trainer.query.filter_by(email=post_trainer_data["email"]).first()
     assert trainer is None
 
-    res = client.post("/trainer", json=post_trainer_data)
+    # Creation success
+    res = client.post("/api/trainer", json=post_trainer_data)
     assert res.status_code == 201
+
+    # Check `trainer` in `db`
     trainer = Trainer.query.filter_by(email=post_trainer_data["email"]).first()
     assert trainer
+
+    # Check duplicate
+    res = client.post("/api/trainer", json=post_trainer_data)
+    assert res.status_code == 400
+    assert "Existing trainer" in res.text
+
+
+def test_handle_trainer_get_method(client, logged_trainer):
+    res = client.get(
+        f"/api/trainer/{logged_trainer.id}",
+        headers={"Authorization": f"Bearer {logged_trainer.token}"},
+    )
+
+    assert res.status_code == 200
+    assert res.json["email"] == logged_trainer.email
+
+
+def test_handle_trainer_put_method(client, logged_trainer):
+    pre = logged_trainer.email
+    res = client.put(
+        f"/api/trainer/{logged_trainer.id}",
+        json=dict(email="x@x.co"),
+        headers={"Authorization": f"Bearer {logged_trainer.token}"},
+    )
+
+    assert res.status_code == 200
+    assert pre != logged_trainer.email
+
+
+def test_handle_trainer_del_method(client, logged_trainer):
+    res = client.delete(
+        f"/api/trainer/{logged_trainer.id}",
+        headers={"Authorization": f"Bearer {logged_trainer.token}"},
+    )
+    assert res.status_code == 200
